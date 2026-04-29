565de32af96427fbf1698536c3235bb9877d71ea
Alfred254-bj <280379640+Alfred254-bj@users.noreply.github.com>
2026-04-29 23:52:31 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
-
-const express = require("express");
+ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("CV Builder API is live 🚀");
 });
 
-const PORT = process.env.PORT || 10000;
+app.get("/health", (req, res) => {
+  res.send("OK");
+});
+
+// IMPORTANT FOR RENDER
+const PORT = process.env.PORT;
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log("Server running on port " + PORT);
